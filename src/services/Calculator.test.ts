250217d6e4526c6b9c0279c062657afaa36fcfb4
Tim Thompson <scubatim84@gmail.com>
2022-03-25 21:15:45 -0600

    Handle invalid input passed to calculateNextLevelCost

--- src/services/Calculator.test.ts
+++ src/services/Calculator.test.ts
@@ -1,5 +1,11 @@
 import { calculateNextLevelCost } from './Calculator';
 
+test('An invalid player level less than 1 throws an exception', () => {
+    const playerLevel = 0;
+
+    expect(() => calculateNextLevelCost(playerLevel)).toThrow('Current level is invalid. Level must be at least 1 and less than or equal to 85.');
+});
+
 test('The correct cost is returned to go from level 1 to 2', () => {
     const playerLevel = 1;
     const nextLevelCost = calculateNextLevelCost(playerLevel);
@@ -26,4 +32,10 @@ test('A cost of 0 is returned when player is at max level of 85', () => {
     const nextLevelCost = calculateNextLevelCost(playerLevel);
 
     expect(nextLevelCost).toBe(0);
+});
+
+test('An invalid player level greater than 85 throws an exception', () => {
+    const playerLevel = 86;
+
+    expect(() => calculateNextLevelCost(playerLevel)).toThrow('Current level is invalid. Level must be at least 1 and less than or equal to 85.');
 });
