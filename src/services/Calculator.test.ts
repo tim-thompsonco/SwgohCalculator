fbdb0eea46ef6c3db26476d4c92fa5d85b8f6ca4
Tim Thompson <scubatim84@gmail.com>
2022-03-26 08:57:02 -0600

    Implement linter and apply rules

--- src/services/Calculator.test.ts
+++ src/services/Calculator.test.ts
@@ -1,41 +1,43 @@
 import { calculateNextLevelCost } from './Calculator';
 
 test('An invalid player level less than 1 throws an exception', () => {
-    const playerLevel = 0;
+  const playerLevel = 0;
 
-    expect(() => calculateNextLevelCost(playerLevel)).toThrow('Current level is invalid. Level must be at least 1 and less than or equal to 85.');
+  expect(() => calculateNextLevelCost(playerLevel))
+    .toThrow('Current level is invalid. Level must be at least 1.');
 });
 
 test('The correct cost is returned to go from level 1 to 2', () => {
-    const playerLevel = 1;
-    const nextLevelCost = calculateNextLevelCost(playerLevel);
+  const playerLevel = 1;
+  const nextLevelCost = calculateNextLevelCost(playerLevel);
 
-    expect(nextLevelCost).toBe(213);
+  expect(nextLevelCost).toBe(213);
 });
 
 test('The correct cost is returned to go from level 29 to 30', () => {
-    const playerLevel = 29;
-    const nextLevelCost = calculateNextLevelCost(playerLevel);
+  const playerLevel = 29;
+  const nextLevelCost = calculateNextLevelCost(playerLevel);
 
-    expect(nextLevelCost).toBe(2982);
+  expect(nextLevelCost).toBe(2982);
 });
 
 test('The correct cost is returned to go from level 84 to 85', () => {
-    const playerLevel = 84;
-    const nextLevelCost = calculateNextLevelCost(playerLevel);
+  const playerLevel = 84;
+  const nextLevelCost = calculateNextLevelCost(playerLevel);
 
-    expect(nextLevelCost).toBe(355071);
+  expect(nextLevelCost).toBe(355071);
 });
 
 test('A cost of 0 is returned when player is at max level of 85', () => {
-    const playerLevel = 85;
-    const nextLevelCost = calculateNextLevelCost(playerLevel);
+  const playerLevel = 85;
+  const nextLevelCost = calculateNextLevelCost(playerLevel);
 
-    expect(nextLevelCost).toBe(0);
+  expect(nextLevelCost).toBe(0);
 });
 
 test('An invalid player level greater than 85 throws an exception', () => {
-    const playerLevel = 86;
+  const playerLevel = 86;
 
-    expect(() => calculateNextLevelCost(playerLevel)).toThrow('Current level is invalid. Level must be at least 1 and less than or equal to 85.');
+  expect(() => calculateNextLevelCost(playerLevel))
+    .toThrow('Current level is invalid. Level must be less than or equal to 85.');
 });
--- src/services/Calculator.ts
+++ src/services/Calculator.ts
@@ -1,14 +1,18 @@
-import { levelingCosts } from "../constants/LevelingCost";
+import { levelingCosts } from '../constants/LevelingCost';
 
 export const calculateNextLevelCost = (currentLevel: number): number => {
-    if (currentLevel < 1 || currentLevel > 85) {
-        throw new Error('Current level is invalid. Level must be at least 1 and less than or equal to 85.')
-    }
+  if (currentLevel < 1) {
+    throw new Error('Current level is invalid. Level must be at least 1.');
+  }
 
-    // 85 is the current max level in SWGOH
-    if (currentLevel === 85) {
-        return 0;
-    }
+  if (currentLevel > 85) {
+    throw new Error('Current level is invalid. Level must be less than or equal to 85.');
+  }
 
-    return levelingCosts[currentLevel];
-}
+  // 85 is the current max level in SWGOH
+  if (currentLevel === 85) {
+    return 0;
+  }
+
+  return levelingCosts[currentLevel];
+};
