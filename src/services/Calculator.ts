da18f3dc69b4f9c809cf3dff58c2e4dc0f99d7d0
Tim Thompson <scubatim84@gmail.com>
2022-03-26 21:47:51 -0600

    Refactor existing logic, add function to calculate cost to level player from X to Y level

--- src/services/Calculator.ts
+++ src/services/Calculator.ts
@@ -1,18 +1,26 @@
 import { levelingCosts } from '../constants/LevelingCost';
+import { maxLevel } from '../constants/LevelInfo';
+import { validatePlayerLevel } from './Validation';
 
 export const calculateNextLevelCost = (currentLevel: number): number => {
-  if (currentLevel < 1) {
-    throw new Error('Current level is invalid. Level must be at least 1.');
-  }
+  validatePlayerLevel(currentLevel);
 
-  if (currentLevel > 85) {
-    throw new Error('Current level is invalid. Level must be less than or equal to 85.');
-  }
-
-  // 85 is the current max level in SWGOH
-  if (currentLevel === 85) {
+  if (currentLevel === maxLevel) {
     return 0;
   }
 
   return levelingCosts[currentLevel];
+};
+
+export const calculateLevelingCost = (startingLevel: number, desiredLevel: number) => {
+  validatePlayerLevel(startingLevel);
+  validatePlayerLevel(desiredLevel);
+
+  let totalCost = 0;
+  for (let i = startingLevel; i < desiredLevel; i++) {
+    const currentLevel = i;
+    totalCost += calculateNextLevelCost(currentLevel);
+  }
+
+  return totalCost;
 };
