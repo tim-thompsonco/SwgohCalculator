b56b31bb9d73313cb6b2d892a48542a03f216fdf
Tim Thompson <45145621+tim-thompsonco@users.noreply.github.com>
2022-03-28 21:17:51 -0600

    Add Material UI (#25)
    * Set up Material UI
    
    * Fix linting for React components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
+import { render } from '@testing-library/react';
 
-test('renders learn react link', () => {
+test('Renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
