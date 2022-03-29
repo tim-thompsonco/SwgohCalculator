61a2dc271c46ecfa52c852637e46cb085b74528c
Tim Thompson <scubatim84@gmail.com>
2022-03-28 21:14:10 -0600

    Fix linting for React components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material';
 const App = () => {
   return (
     <Typography variant="h2" color="primary.main">
-    Hello World
+      Hello World
     </Typography>
   );
 };
