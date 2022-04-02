d113596cffbd3b5621dfd568a8588b23ba2f8d59
Tim Thompson <scubatim84@gmail.com>
2022-04-02 08:18:43 -0600

    Create CalculatorSelect component

---
+++ src/components/CalculatorSelect.tsx
@@ -0,0 +1,67 @@
+import {
+  FormControl, Grid, MenuItem, Select, Typography, makeStyles 
+} from '@material-ui/core';
+import React, { ChangeEvent } from 'react';
+import { convertLabelToId } from '../utilities/Utilities';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  formControl: {
+    backgroundColor: theme.palette.secondary.main,
+    margin: theme.spacing(1, 0)
+  },
+  levelLabel: {
+    color: theme.palette.primary.main,
+    fontSize: 18,
+  },
+  levelSelect: {
+    background: theme.palette.secondary.main,
+    paddingBottom: theme.spacing(2),
+    height: theme.spacing(3),
+    width: theme.spacing(8)
+  },
+  rightAlignItem: { justifyItems: 'flex-end' }
+}));
+
+interface ICalculatorSelectProps {
+    handleUpgrade(e: ChangeEvent<{ value: unknown }>):  void,
+    upgradeCosts: Record<number, number>,
+    upgradeLabel: string,
+    upgradeValue: number
+}
+
+const CalculatorSelect: React.FC<ICalculatorSelectProps> = ({
+  handleUpgrade, upgradeCosts, upgradeLabel, upgradeValue 
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Grid className={classes.container} container>
+      <Grid item xs={2}>
+        <Typography className={classes.levelLabel}>
+          {upgradeLabel}
+        </Typography>
+      </Grid>
+      <Grid className={classes.rightAlignItem} item xs={1}>
+        <FormControl className={classes.formControl} variant="filled">
+          <Select
+            className={classes.levelSelect}
+            defaultValue={1}
+            id={convertLabelToId(upgradeLabel)}
+            onChange={handleUpgrade}
+            value={upgradeValue}
+          >
+            {Object.keys(upgradeCosts).map((upgradeCost: string) => {
+              return <MenuItem key={upgradeCost} value={upgradeCost}>{upgradeCost}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CalculatorSelect;
