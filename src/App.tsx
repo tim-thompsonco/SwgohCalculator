e87fb35b79d57d3c688cdfc76e0c2980a747e172
Tim Thompson <scubatim84@gmail.com>
2022-04-21 09:35:41 -0600

    Set up GA in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+
 import { CircularProgress } from '@material-ui/core';
 import React, { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import ReactGA from 'react-ga';
 import { Provider } from 'react-redux';
 
 import { ErrorFallback } from './components/index';
 import { store } from './store';
+
+const googleTrackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID as string;
+ReactGA.initialize(googleTrackingId);
 
 const Calculator = React.lazy(() => import('./components/index').then(module => ({ default: module.Calculator })));
 
