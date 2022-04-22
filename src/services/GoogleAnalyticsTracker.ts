461a82430bb7834db6f047006a631a610bbd0860
Tim Thompson <scubatim84@gmail.com>
2022-04-22 08:27:14 -0600

    Replace React-GA with React-GA4

--- src/services/GoogleAnalyticsTracker.ts
+++ src/services/GoogleAnalyticsTracker.ts
@@ -1,4 +1,4 @@
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 export const logGoogleAnalyticsEvent = (category: string, action: string, label: string) => {
   return ReactGA.event({ category, action, label });
