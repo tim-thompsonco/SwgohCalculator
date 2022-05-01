17351dc1850bd9169b52aa12ff87376b5692da52
Tim Thompson <45145621+tim-thompsonco@users.noreply.github.com>
2022-05-01 10:09:57 -0600

    Make logo responsive (#60)

--- src/components/AppLogo/AppLogo.tsx
+++ src/components/AppLogo/AppLogo.tsx
@@ -1,12 +1,17 @@
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
 import SwgohCalculatorLogo from '../../assets/images/logo.png';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   logo: {
-    height: 100,
-    width: 1000
+    maxHeight: 'auto',
+    maxWidth: theme.breakpoints.down('sm') ? '100%' : '50%'
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: theme.breakpoints.down('sm') ? 'center' : 'flex-start'
   }
 }));
 
@@ -14,7 +19,11 @@ const AppLogo: React.FC = () => {
   const classes = useStyles();
     
   return (
-    <img className={classes.logo} src={SwgohCalculatorLogo} />
+    <Grid className={classes.logoContainer} container>
+      <Grid item xs={12}>
+        <img className={classes.logo} src={SwgohCalculatorLogo} />
+      </Grid>
+    </Grid>
   );
 };
 
