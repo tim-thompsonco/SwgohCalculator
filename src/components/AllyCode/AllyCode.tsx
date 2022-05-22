007d42cc5ab9fdfff46dd4d8269437f09d024932
Tim Thompson <scubatim84@gmail.com>
2022-05-21 19:27:36 -0600

    Implement logic to fetch player roster

--- src/components/AllyCode/AllyCode.tsx
+++ src/components/AllyCode/AllyCode.tsx
@@ -1,5 +1,9 @@
-import { Button, Grid, makeStyles, TextField } from '@material-ui/core';
-import React from 'react';
+import { Button, CircularProgress, Grid, makeStyles, TextField } from '@material-ui/core';
+import React, { ChangeEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { hydratePlayerRoster, PlayerUnit } from '../../features/PlayerRosterSlice';
+import api from '../../services/Axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,19 +16,44 @@ const useStyles = makeStyles((theme) => ({
 
 const AllyCode: React.FC = () => {
   const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(false);
+  const [allyCode, setAllyCode] = useState(0);
+  const dispatch = useDispatch();
+
+  const getPlayerRoster = async () => {
+    setIsLoading(true);
+    const response = await api.get(`/roster/${allyCode}`);
+    const playerRoster: Record<string, PlayerUnit> = response.data;
+  
+    dispatch(hydratePlayerRoster(playerRoster));
+    setIsLoading(false);
+  };
+
+  const changeAllyCode = (event: ChangeEvent<{ value: unknown }>) => {
+    setAllyCode(event.target.value as number);
+  };
 
   return (
     <Grid className={classes.root} container spacing={2}>
       <Grid item xs={8}>
         <TextField
           id={'AllyCodeTextField'}
+          onChange={changeAllyCode}
           placeholder={'Enter ally code'}
           size={'small'}
           variant={'outlined'}
         />
       </Grid>
       <Grid item xs={4}>
-        <Button color={'secondary'} variant={'contained'}>Go</Button>
+        {isLoading ?
+          <CircularProgress color={'secondary'} />
+          :
+          <Button 
+            color={'secondary'} 
+            onClick={getPlayerRoster}
+            variant={'contained'}
+          >Go
+          </Button>}
       </Grid>
     </Grid>
   );
